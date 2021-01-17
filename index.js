f259d1cb0ac73f30c783b803808d6160a6badeff
Rob <morrisrjc@gmail.com>
2021-01-17 21:44:03 +0000

    Finished core will com back for stretch

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
-let balance = 500.00;
 
-class Withdrawal {
 
-  constructor(amount) {
-    this.amount = amount;
-  }
 
-  commit() {
-    balance -= this.amount;
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.balance = 1000.00;
   }
 
 }
 
+class Transaction {
 
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
 
+  commit() {
+    this.account.balance += this.value;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
 
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('Sirrom84');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(1000.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
+console.log(`New deposit:`, t1.amount);
+console.log(`Updated Balance:`, myAccount.balance)
+const t2 = new Withdrawal(160.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
